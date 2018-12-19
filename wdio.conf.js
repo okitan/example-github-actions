676942a5b38c469db777d7aa6ed76907e3bb6681
okitan <okitakunio@gmail.com>
2018-12-19 19:56:22 +0900

    :bulb:

--- wdio.conf.js
+++ wdio.conf.js
@@ -19,7 +19,7 @@ exports.config = {
         name: "chrome" // capabilitiy name (mainly use for screenshot)
       },
       "goog:chromeOptions": {
-        args: ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
+        args: ["--headless"]
       }
     }
     // {
